18103f7ea62e14afeb035b48b686c7600b988580
maram-ghouma <ghoumamaram@gmail.com>
2024-05-06 17:27:30 +0300

    product details page added

--- src/Controller/BaseController.php
+++ src/Controller/BaseController.php
@@ -5,14 +5,33 @@ namespace App\Controller;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
+use App\Repository\ProductRepository;
 
 class BaseController extends AbstractController
 {
-    #[Route('/base', name: 'app_base')]
-    public function index(): Response
+    #[Route('/shop', name: 'app_base')]
+    public function index(ProductRepository $productRepository): Response
     {
-        return $this->render('base.html.twig', [
-            'controller_name' => 'BaseController',
+        $menProducts = $productRepository->findByCategory('Men sportswear');
+        $womenProducts = $productRepository->findByCategory('Women sportswear');
+        $supplements = $productRepository->findByCategory('Supplements');
+
+        return $this->render('base/index.html.twig', [
+            'menProducts' => $menProducts,
+            'womenProducts' => $womenProducts,
+            'supplements' => $supplements,
+        ]);
+    }
+    #[Route('/product/{id}', name: 'product_details')]
+    public function show($id, ProductRepository $productRepository): Response
+    {
+        $product = $productRepository->find($id);
+        if (!$product) {
+            throw $this->createNotFoundException('Product not found');
+        }
+
+       return $this->render('base/product_details.html.twig', [
+            'product' => $product,
         ]);
     }
     #[Route('/base/contact', name: 'contact')]
@@ -21,3 +40,4 @@ class BaseController extends AbstractController
         return $this->redirectToRoute('form.add');
     }
 }
+
